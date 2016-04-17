a2087638bc876010ca06c3675dbabbbb13c3e95f
mavidser <mavidser@gmail.com>
2016-04-17 11:23:19 +0530

    I don't remember what changed

--- app/src/main/java/mavidser/textemoticons/EmoticonIME.java
+++ app/src/main/java/mavidser/textemoticons/EmoticonIME.java
@@ -1,15 +1,16 @@
 package mavidser.textemoticons;
 
+import android.content.Context;
 import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.media.AudioManager;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
+import android.view.inputmethod.InputMethodManager;
 
 /**
  * Created by sid on 22/2/16.
@@ -19,18 +20,25 @@ public class EmoticonIME extends InputMethodService
 
     private KeyboardView kv;
     private Keyboard keyboard;
+    InputMethodManager imeManager;
+    IBinder token;
+    private Keyboard tablesKeyboard;
+    private Keyboard facesKeyboard;
 
     private boolean caps = false;
-
+    private int mKeyboardState;
     @Override
     public View onCreateInputView() {
+        imeManager = (InputMethodManager)this.getSystemService(Context.INPUT_METHOD_SERVICE);
         kv = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard, null);
-        keyboard = new Keyboard(this, R.xml.qwerty);
-        kv.setKeyboard(keyboard);
+        keyboard = new Keyboard(this, R.xml.emoticon_main);
+        tablesKeyboard= new Keyboard(this, R.xml.emoticon_main, R.integer.table);
+        facesKeyboard = new Keyboard(this, R.xml.emoticon_main, R.integer.faces);
+        mKeyboardState = R.integer.faces;
+        token = this.getWindow().getWindow().getAttributes().token;
+        kv.setKeyboard(facesKeyboard);
         kv.setOnKeyboardActionListener(this);
         kv.setPreviewEnabled(false);
-        kv.setHorizontalScrollBarEnabled(true);
-        kv.canScrollHorizontally(1);
         return kv;
     }
 
@@ -53,25 +61,24 @@ public class EmoticonIME extends InputMethodService
 
     @Override
     public void onKey(int primaryCode, int[] keyCodes) {
+        Log.d("Key Pressed", primaryCode+"");
         InputConnection ic = getCurrentInputConnection();
         playClick(primaryCode);
         switch(primaryCode){
             case Keyboard.KEYCODE_DELETE :
                 ic.deleteSurroundingText(1, 0);
                 break;
-            case Keyboard.KEYCODE_SHIFT:
-                caps = !caps;
-                keyboard.setShifted(caps);
-                kv.invalidateAllKeys();
+            case -10:
+                imeManager.switchToNextInputMethod(token, false);
                 break;
-            case Keyboard.KEYCODE_DONE:
-                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+            case -100:
+                kv.setKeyboard(facesKeyboard);
+                break;
+            case -101:
+                kv.setKeyboard(tablesKeyboard);
                 break;
             default:
                 char code = (char)primaryCode;
-                if(Character.isLetter(code) && caps){
-                    code = Character.toUpperCase(code);
-                }
                 ic.commitText(String.valueOf(code),1);
         }
     }
@@ -86,6 +93,9 @@ public class EmoticonIME extends InputMethodService
 
     @Override
     public void onText(CharSequence text) {
+        Log.d("Text Received", text.toString());
+        InputConnection ic = getCurrentInputConnection();
+        ic.commitText(text,1);
     }
 
     @Override
